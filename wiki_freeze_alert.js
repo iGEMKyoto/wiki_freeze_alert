8a411c2813c4d58ab2c8fd9df9af1cabbd3977e3
Benijake <benijake.benijake@gmail.com>
2014-05-24 00:17:08 +0900

    added some basic functions

--- wiki_freeze_alert.js
+++ wiki_freeze_alert.js
@@ -13,6 +13,30 @@ function get_current_utc() {
     );
 }
 
+/*
+ * return array of the quoteient and remainder
+ */
+function qr(dividend, divisor) {
+    return [Math.floor(dividend/divisor),
+        dividend%divisor];
+}
+
+function repeat_str(str, n) {
+    var retval = '';
+    for (var i = 0; i < n; i++) {
+        retval += str;
+    }
+    return retval;
+}
+
+/*
+ * transfer Integer to String
+ */
+function itos(number, figures) {
+    return [repeat_str('0', figures-1),number]
+        .join('').slice(-figures);
+}
+
 var timer = setInterval(function() {
     var countdown = document.getElementById("kyoto_alert");
     var current_utc = get_current_utc();
@@ -25,23 +49,23 @@ var timer = setInterval(function() {
         else if (interval < 24 * 3600)
             countdown.style.color = "#ff0";
 
-        var second = ('0' + (interval % 60)).slice(-2) + 's';
-        interval = Math.floor(interval / 60);
-        var minute = ('0' + (interval % 60)).slice(-2) + 'm';
-        interval = Math.floor(interval / 60);
-        var hour = ('0' + (interval)).slice(-2) + 'h';
-        var str = hour + " : " + minute + " : " + second;
-        countdown.innerText = str;
+        var time = [];
+        var second_qr = qr(interval, 60);
+        time.push(itos(second_qr[1], 2) + 's');
+        var minute_qr = qr(second_qr[0], 60);
+        time.push(itos(minute_qr[1], 2) + 'm');
+        time.push(itos(minute_qr[0], 2) + 'h');
+        countdown.innerText = time.reverse().join(' : ');
         countdown.style.display = "block";
     } else {
+        var time = [];
         interval = Math.floor(interval / 60);
-        var minute = ('0' + (interval % 60)).slice(-2) + 'm';
-        interval = Math.floor(interval / 60);
-        var hour = ('0' + (interval)).slice(-2) + 'h';
-        interval = Math.floor(interval / 24);
-        var date = ('00' + (interval)).slice(-3) + 'd';
-        var str = date + " : " + hour + " : " + minute;
-        countdown.innerText = str;
+        var minute_qr = qr(interval, 60);
+        time.push(itos(minute_qr[1], 2) + 'm');
+        var hour_qr = qr(minute_qr[0], 24);
+        time.push(itos(hour_qr[1], 2) + 'h');
+        time.push(itos(hour_qr[0], 3) + 'd');
+        countdown.innerText = time.reverse().join(' : ');
         countdown.style.display = "block";
     }
 }, 200);
