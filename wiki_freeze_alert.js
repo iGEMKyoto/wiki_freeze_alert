72141725a531bdfc420bcedab28047c3d0039773
benijake <benijake.benijake@gmail.com>
2014-09-07 01:56:27 +0900

    Updated wiki freeze

--- wiki_freeze_alert.js
+++ wiki_freeze_alert.js
@@ -1,7 +1,7 @@
 $(function(){
 
 /* October 17 wiki FREEZE at 11:59PM EDT */
-var freeze = Date.UTC(2014, 9, 17, 3, 59, 0);
+var freeze = Date.UTC(2014, 9, 10, 3, 59, 0);
 
 function get_current_utc() {
     var date = new Date();
