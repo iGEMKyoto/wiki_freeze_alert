91dd1f2114234d445929de4f3b244daaf8fd19a5
benijake <benijake.benijake@gmail.com>
2014-09-11 08:29:43 +0900

    made this movable and visible in FireFox 32.0

--- wiki_freeze_alert.js
+++ wiki_freeze_alert.js
@@ -1,7 +1,10 @@
 $(function(){
 
-/* October 17 wiki FREEZE at 11:59PM EDT */
-var freeze = Date.UTC(2014, 9, 10, 4, 00, 0);
+/* October 17 wiki FREEZE at 12:00PM EDT */
+var year = 2014;
+var month = 10;
+var day = 10;
+var freeze = Date.UTC(year, month-1, day, 4, 00, 0);
 
 function get_current_utc() {
     var date = new Date();
@@ -40,16 +43,17 @@ function itos(number, figures) {
 }
 
 var timer = setInterval(function() {
-    var countdown = document.getElementById("kyoto_alert");
+    //var countdown = document.getElementById("kyoto_alert");
+    var $countdown = $("#kyoto_alert");
     var current_utc = get_current_utc();
     var interval = (freeze - current_utc)/1000;
     if (interval < 0) {
-        countdown.style.display = "none";
+        $countdown.hide();
     } else if (interval < 4 * 24 * 3600) {
         if (interval < 1 * 3600)
-            countdown.style.color = "#f00";
+            $countdown.css("color", "#f00");
         else if (interval < 24 * 3600)
-            countdown.style.color = "#ff0";
+            $countdown.css("color", "#ff0");
 
         var time = [];
         var second_qr = qr(interval, 60);
@@ -57,8 +61,8 @@ var timer = setInterval(function() {
         var minute_qr = qr(second_qr[0], 60);
         time.push(itos(minute_qr[1], 2) + 'm');
         time.push(itos(minute_qr[0], 2) + 'h');
-        countdown.innerText = time.reverse().join(' : ');
-        countdown.style.display = "block";
+        $countdown.text(time.reverse().join(' : '));
+        $countdown.show();
     } else {
         var time = [];
         interval = Math.floor(interval / 60);
@@ -67,9 +71,38 @@ var timer = setInterval(function() {
         var hour_qr = qr(minute_qr[0], 24);
         time.push(itos(hour_qr[1], 2) + 'h');
         time.push(itos(hour_qr[0], 3) + 'd');
-        countdown.innerText = time.reverse().join(' : ');
-        countdown.style.display = "block";
+        $countdown.text(time.reverse().join(' : '));
+        $countdown.show();
     }
 }, 200);
+
+var kyoto_alert_wrapper = {
+    x : undefined,
+    y : undefined,
+    is_down : false
+};
+
+$("#kyoto_alert").mousedown(function(e) {
+    kyoto_alert_wrapper.x = e.pageX;
+    kyoto_alert_wrapper.y = e.pageY;
+    kyoto_alert_wrapper.is_down = true;
+}).mousemove(function(e) {
+    if (kyoto_alert_wrapper.is_down &&
+        kyoto_alert_wrapper.x !== undefined &&
+        kyoto_alert_wrapper.y !== undefined) {
+        var x = e.pageX,
+            y = e.pageY;
+        var offset = $(this).offset();
+        offset.left += x - kyoto_alert_wrapper.x;
+        offset.top += y - kyoto_alert_wrapper.y;
+        $(this).offset(offset);
+        kyoto_alert_wrapper.x = x;
+        kyoto_alert_wrapper.y = y;
+    }
+}).mouseup(function(e) {
+    kyoto_alert_wrapper.x = undefined;
+    kyoto_alert_wrapper.y = undefined;
+    kyoto_alert_wrapper.is_down = false;
+});
 
 });
